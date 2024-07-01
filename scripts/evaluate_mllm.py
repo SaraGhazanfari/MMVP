f79625ce04df581293927ebb86696901f56c8648
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-07-01 14:25:27 -0400

    first commit

--- scripts/evaluate_mllm.py
+++ scripts/evaluate_mllm.py
@@ -95,6 +95,8 @@ def eval_model(args):
                 use_cache=True)
 
         input_token_len = input_ids.shape[1]
+        print(input_ids.shape)
+        print(output_ids.shape)
         n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
         if n_diff_input_output > 0:
             print(f'[Warning] {n_diff_input_output} output_ids are not the same as the input_ids')
