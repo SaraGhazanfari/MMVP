289b291fcf3f8bf562747897117ead6edac9a496
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-07-01 17:38:25 -0400

    first commit

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -72,13 +72,14 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             else:
                 # this is probably from HF Hub
                 from huggingface_hub import hf_hub_download
-
+                cache_dir='$SCRATCH/code/mmvp'
+                os.environ['HF_HOME'] = cache_dir
                 def load_from_hf(repo_id, filename, subfolder=None):
                     cache_file = hf_hub_download(
                         repo_id=repo_id,
                         filename=filename,
                         subfolder=subfolder,
-                        cache_dir='$SCRATCH/code/mmvp', local_dir='$SCRATCH/code/mmvp')
+                        cache_dir=cache_dir)
                     return torch.load(cache_file, map_location='cpu')
 
                 non_lora_trainables = load_from_hf(model_path, 'non_lora_trainables.bin')
